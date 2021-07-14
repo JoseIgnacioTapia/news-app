68b56d4fc313d305761b6b4c6dfd07a1c6ff4efc
JoseIgnacioTapia <josignaciot@gmail.com>
2021-07-14 17:53:33 -0300

    Warnings resolved

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -1,6 +1,5 @@
 import React from 'react';
-// import './Showcase.css';
-import styled from 'styled-components';
+import './Showcase.css';
 
 const Showcase = ({ optionSelected, data }) => {
   // Extracting Information
@@ -9,46 +8,20 @@ const Showcase = ({ optionSelected, data }) => {
   let category =
     optionSelected.charAt(0).toUpperCase() + optionSelected.slice(1);
 
-  const Header = styled.header`
-    color: #fff;
-    background: #333;
-    padding: 2rem;
-    position: relative;
-
-    .showcase-container {
-      display: grid;
-      grid-template-columns: repeat(2, 1fr);
-      justify-content: center;
-      align-items: center;
-      height: 50vh;
-
-      .showcase-content {
-        z-index: 1;
-
-        p {
-          margin-bottom: 1rem;
-        }
-      }
-    }
-
-    &:before {
-      content: '';
-      background: url(${urlToImage}) no-repeat center center/cover;
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      opacity: 0.4;
-    }
-  `;
+  let styles = {
+    backgroundImage: `url(${urlToImage})`,
+    backgroundColor: '',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+  };
 
   return (
-    <Header>
+    <div style={styles} className="showcase">
       <div className="container">
         <div className="showcase-container">
           <div className="showcase-content">
-            <div className="category category-sports">{category}</div>
+            <div className="category ">{category}</div>
             <h1>{title}</h1>
             <p>{description}</p>
             <a href={url} className="btn btn-primary" target="_blank">
@@ -57,7 +30,7 @@ const Showcase = ({ optionSelected, data }) => {
           </div>
         </div>
       </div>
-    </Header>
+    </div>
   );
 };
 
