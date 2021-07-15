18f8fd1eb2fe6e2b6fea4c8a236c1bbe2199cc39
JoseIgnacioTapia <josignaciot@gmail.com>
2021-07-15 15:58:56 -0300

    Fixing problem on Custom Hook

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -1,30 +1,41 @@
 import React from 'react';
 import './Showcase.css';
+import { useCategory } from '../hooks/useCategory';
 import { capitalize } from '../helpers/capitalize';
 
 const Showcase = ({ optionSelected, data }) => {
   // Extracting Information
   const { urlToImage, url, title, description, source } = data;
 
+  // Colours by category
+
   let category = capitalize(optionSelected);
 
   let styles = {
     backgroundImage: `url(${urlToImage})`,
-    backgroundColor: '',
     backgroundPosition: 'center',
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
   };
+
+  // Custom Hook
+  const [state, Category] = useCategory(category, optionSelected);
 
   return (
     <div style={styles} className="showcase">
       <div className="container">
         <div className="showcase-container">
           <div className="showcase-content">
-            <div className="category ">{category}</div>
+            {/* <div className="category ">{category}</div> */}
+            <Category />
             <h1>{title}</h1>
             <p>{description}</p>
-            <a href={url} className="btn btn-primary" target="_blank">
+            <a
+              href={url}
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer"
+            >
               Read More
             </a>
           </div>
---
+++ src/hooks/useCategory.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+
+const colours = {
+  general: '#577590',
+  business: '#43aa8b',
+  entertainment: '#f94144',
+  health: '#90be6d',
+  science: '#f8961e',
+  sports: '#f9c74f',
+  technology: '#f3722c',
+};
+
+export const useCategory = (categoryTitle, category) => {
+  const [state, setState] = useState(category);
+
+  useEffect(() => {
+    setState(category);
+  }, [category]);
+
+  let styles = {
+    display: 'inline-block',
+    backgroundColor: colours[state],
+    color: '#fff',
+    fontSize: '0.55rem',
+    textTransform: 'uppercase',
+    padding: '0.4rem 0.6rem',
+    borderRadius: '15px',
+    marginBottom: '0.5rem',
+  };
+
+  const Category = () => <div style={styles}>{categoryTitle}</div>;
+
+  return [state, Category];
+};
